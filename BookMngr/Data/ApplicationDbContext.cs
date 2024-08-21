a9bb16c88619dcde4f9d46500558130faf6d376d
tshepisorankholo <rankholotshepiso@gmail.com>
2024-08-21 17:54:43 +0200

    Project Setup

--- BookMngr/Data/ApplicationDbContext.cs
+++ BookMngr/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookMngr.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
